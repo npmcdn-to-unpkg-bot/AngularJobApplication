963be579ea37d022d16ce539d79b8770eed3d83e
Eric Ingland <ingland.eric@gmail.com>
2016-03-21 22:00:54 -0500

    Integrate and Update Submission php
    With Database update and Angular

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,6 +1,6 @@
 import {Component} from 'angular2/core';
 @Component({
     selector: 'my-app',
-    template: '<h1>My SECOND Angular 2 App</h1>'
+    template: '<h1>My Job Application Angular 2 App</h1>'
 })
 export class AppComponent { }
--- index.php
+++ index.php
@@ -9,17 +9,18 @@ if (!empty($_POST)) {
         // set the PDO error mode to exception
         $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         
-        $stmt = $conn->prepare("INSERT INTO Applicants (firstname, lastname, email, telephone, comments) VALUES (:firstname, :lastname, :email, :telephone, :comments)");
+        $stmt = $conn->prepare("INSERT INTO Users (email, password, firstname, lastname, telephone, address, city, postcode) VALUES (:email, :password, :firstname, :lastname, :telephone, :address, :city, :postcode)");
+        $stmt->bindParam(':email', $_POST['email']);
+        $stmt->bindParam(':password', $_POST['email']);
         $stmt->bindParam(':firstname', $_POST['firstname']);
         $stmt->bindParam(':lastname', $_POST['lastname']);
-        $stmt->bindParam(':email', $_POST['email']);
         $stmt->bindParam(':telephone', $_POST['telephone']);
-        $stmt->bindParam(':comments', $_POST['comments']);
+        $stmt->bindParam(':address', $_POST['address']);
+        $stmt->bindParam(':city', $_POST['city']);
+        $stmt->bindParam(':postcode', $_POST['postcode']);
         $stmt->execute();
-        echo "Submitted successfully";
-        
-        
-        
+        echo "Submitted successfully";  
+
         $conn = null;
         }
     catch(PDOException $e)
@@ -29,104 +30,120 @@ if (!empty($_POST)) {
 }
 
 ?>
+<!doctype html>
+<html>
+  <head>
+    <title>Job Application Submission</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">    
+    <link rel="stylesheet" href="styles.css">
 
+    <!-- 1. Load libraries -->
+    <!-- IE required polyfills, in this exact order -->
+    <script src="node_modules/es6-shim/es6-shim.min.js"></script>
+    <script src="node_modules/systemjs/dist/system-polyfills.js"></script>
+    <script src="node_modules/angular2/es6/dev/src/testing/shims_for_IE.js"></script>   
 
+    <script src="node_modules/angular2/bundles/angular2-polyfills.js"></script>
+    <script src="node_modules/systemjs/dist/system.src.js"></script>
+    <script src="node_modules/rxjs/bundles/Rx.js"></script>
+    <script src="node_modules/angular2/bundles/angular2.dev.js"></script>
+
+    <!-- 2. Configure SystemJS -->
+    <script>
+      System.config({
+        packages: {        
+          app: {
+            format: 'register',
+            defaultExtension: 'js'
+          }
+        }
+      });
+      System.import('app/main')
+            .then(null, console.error.bind(console));
+    </script>
+  </head>
+
+  <!-- 3. Display the application -->
+  <body>
+    <my-app>Loading...</my-app>
+
+<b>User Registration</b>
+  
 <form name="contactform" method="post" action="index.php">
- 
 <table width="450px">
- 
 <tr>
- 
  <td valign="top">
- 
-  <label for="firstname">First Name *</label>
- 
+  <label for="email">Email *</label>
  </td>
- 
  <td valign="top">
- 
-  <input  type="text" name="firstname" maxlength="50" size="30">
- 
- </td>
- 
-</tr>
- 
-<tr>
- 
- <td valign="top"">
- 
-  <label for="lastname">Last Name *</label>
- 
- </td>
- 
- <td valign="top">
- 
-  <input  type="text" name="lastname" maxlength="50" size="30">
- 
- </td>
- 
-</tr>
- 
-<tr>
- 
- <td valign="top">
- 
-  <label for="email">Email Address *</label>
- 
- </td>
- 
- <td valign="top">
- 
   <input  type="text" name="email" maxlength="80" size="30">
- 
  </td>
- 
 </tr>
- 
 <tr>
- 
- <td valign="top">
- 
-  <label for="telephone">Telephone Number</label>
- 
+ <td valign="top"">
+  <label for="password">Password *</label>
  </td>
- 
  <td valign="top">
- 
+  <input  type="text" name="password" maxlength="41" size="30">
+ </td>
+</tr>
+<tr>
+<tr>
+ <td valign="top">
+  <label for="firstname">First Name *</label>
+ </td>
+ <td valign="top">
+  <input  type="text" name="firstname" maxlength="25" size="30">
+ </td>
+</tr>
+<tr>
+ <td valign="top"">
+  <label for="lastname">Last Name *</label>
+ </td>
+ <td valign="top">
+  <input  type="text" name="lastname" maxlength="25" size="30">
+ </td>
+</tr>
+<tr>
+ <td valign="top">
+  <label for="telephone">Telephone *</label>
+ </td>
+ <td valign="top">
   <input  type="text" name="telephone" maxlength="30" size="30">
- 
  </td>
- 
 </tr>
- 
 <tr>
- 
  <td valign="top">
- 
-  <label for="comments">Comments *</label>
- 
+  <label for="address">Address *</label>
  </td>
- 
  <td valign="top">
- 
-  <textarea  name="comments" maxlength="1000" cols="25" rows="6"></textarea>
- 
+  <input  type="text" name="address" maxlength="80" size="30">
  </td>
- 
 </tr>
- 
 <tr>
- 
+ <td valign="top">
+  <label for="city">City *</label>
+ </td>
+ <td valign="top">
+  <input  type="text" name="city" maxlength="30" size="30">
+ </td>
+</tr>
+<tr>
+ <td valign="top">
+  <label for="postcode">Zip Code *</label>
+ </td>
+ <td valign="top">
+  <input  type="text" name="postcode" maxlength="7" size="30">
+ </td>
+</tr>
+<tr>
  <td colspan="2" style="text-align:center">
- 
   <input type="submit" value="Submit">
- 
  </td>
- 
 </tr>
- 
 </table>
- 
 </form>
-
 <a href="admin.php">Admin page</a>
+
+  </body>
+</html>
