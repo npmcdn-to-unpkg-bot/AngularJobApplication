17e153bf80aa3d64a5cd17fca1e53359152a33a6
Eric Ingland <ingland.eric@gmail.com>
2016-03-25 23:31:42 -0500

    Tour of Heroes Tutorial

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,6 +1,49 @@
-import {Component} from 'angular2/core';
+import { Component }       from 'angular2/core';
+import { HeroService }     from './hero.service';
+import { HeroesComponent } from './heroes.component';
+import { HeroDetailComponent } from './hero-detail.component';
+import { DashboardComponent } from './dashboard.component';
+import { RouteConfig, ROUTER_DIRECTIVES, ROUTER_PROVIDERS } from 'angular2/router';
+
 @Component({
     selector: 'my-app',
-    template: '<h1>My Job Application Angular 2 App</h1>'
+    template: `
+    <h1>{{title}}</h1>
+    <nav>
+        <a [routerLink]="['Dashboard']">Dashboard</a>
+        <a [routerLink]="['Heroes']">Heroes</a>
+    </nav>
+    <router-outlet></router-outlet>
+    <my-hero-detail [hero]="selectedHero"></my-hero-detail>
+    `,
+    styleUrls: ['app/app.component.css'],
+    directives: [ROUTER_DIRECTIVES],
+    providers: [
+    ROUTER_PROVIDERS,
+    HeroService
+    ]
 })
-export class AppComponent { }
+
+@RouteConfig([
+  {
+    path: '/dashboard',
+    name: 'Dashboard',
+    component: DashboardComponent,
+    useAsDefault: true
+  },
+  {
+  path: '/detail/:id',
+  name: 'HeroDetail',
+  component: HeroDetailComponent
+  },
+  {
+    path: '/heroes',
+    name: 'Heroes',
+    component: HeroesComponent
+  }
+])
+
+
+export class AppComponent {
+  title = 'Tour of Heroes';
+}
