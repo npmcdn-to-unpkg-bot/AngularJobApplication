4a1a4d3fb0034fd0a5afc2397313d73014c54494
Eric Ingland <ingland.eric@gmail.com>
2016-03-27 19:52:52 -0500

    Switch to Hero Form Base

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,49 +1,16 @@
-import { Component }       from 'angular2/core';
-import { HeroService }     from './hero.service';
-import { HeroesComponent } from './heroes.component';
-import { HeroDetailComponent } from './hero-detail.component';
-import { DashboardComponent } from './dashboard.component';
-import { RouteConfig, ROUTER_DIRECTIVES, ROUTER_PROVIDERS } from 'angular2/router';
+import {Component}         from 'angular2/core';
+import {HeroFormComponent} from './hero-form.component'
 
 @Component({
-    selector: 'my-app',
-    template: `
-    <h1>{{title}}</h1>
-    <nav>
-        <a [routerLink]="['Dashboard']">Dashboard</a>
-        <a [routerLink]="['Heroes']">Heroes</a>
-    </nav>
-    <router-outlet></router-outlet>
-    <my-hero-detail [hero]="selectedHero"></my-hero-detail>
-    `,
-    styleUrls: ['app/app.component.css'],
-    directives: [ROUTER_DIRECTIVES],
-    providers: [
-    ROUTER_PROVIDERS,
-    HeroService
-    ]
+  selector: 'my-app',
+  template: '<hero-form></hero-form>',
+  directives: [HeroFormComponent]
 })
-
-@RouteConfig([
-  {
-    path: '/dashboard',
-    name: 'Dashboard',
-    component: DashboardComponent,
-    useAsDefault: true
-  },
-  {
-  path: '/detail/:id',
-  name: 'HeroDetail',
-  component: HeroDetailComponent
-  },
-  {
-    path: '/heroes',
-    name: 'Heroes',
-    component: HeroesComponent
-  }
-])
+export class AppComponent { }
 
 
-export class AppComponent {
-  title = 'Tour of Heroes';
-}
+/*
+Copyright 2016 Google Inc. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at http://angular.io/license
+*/
