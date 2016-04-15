36002a6382c50327724dfeaada46836625fd8ed1
Eric Ingland <ingland.eric@gmail.com>
2016-04-15 09:04:42 -0500

    Begin recapcha integration

--- submit.php
+++ submit.php
@@ -131,7 +131,27 @@ if (!empty($_POST)) {
 
 // Create and send Email
 
-if(empty($errors))
+// grab recaptcha library
+require_once "recaptchalib.php";
+
+// your secret key
+$secret = "6LelRR0TAAAAAOncS55Sjy526EgLzASHqLfIfZ18";
+ 
+// empty response
+$response = null;
+ 
+// check secret key
+$reCaptcha = new ReCaptcha($secret);
+
+// if submitted check response
+if ($_POST["g-recaptcha-response"]) {
+    $response = $reCaptcha->verifyResponse(
+        $_SERVER["REMOTE_ADDR"],
+        $_POST["g-recaptcha-response"]
+    );
+}
+
+if($response != null && $response->success)
 {
 
     $email = $_POST['email'];
